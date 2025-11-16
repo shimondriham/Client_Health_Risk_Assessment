87b0b67ea09f2d70b311a9e08127a954a5f31f35
Ben7767 <142659132+Ben7767@users.noreply.github.com>
2025-11-16 17:38:13 +0200

    explain change

--- src/componentsClient/explanatoryV.jsx
+++ src/componentsClient/explanatoryV.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
+import { useNavigate } from "react-router-dom";
 
 function ExplanatoryV() {
+ const [checked, setChecked] = useState(false);
+  let nav = useNavigate();
+
+    const NextPage = () => {
+    nav("/Calibration");
+  };
+
   return (
-    <div>ExplanatoryV</div>
-  )
+    <div className="container mt-1 text-center">
+          <img style={{ height: '60px', width: '60px', borderRadius: '50px' }} src="src/assets/react.svg" alt="logo" />
+      <h2>Calibration Video</h2>
+
+      <video 
+      width="720" 
+      controls 
+      className="my-3 rounded border"
+      >
+      <source src="/videos/121212.mp4" type="video/mp4" />
+      הדפדפן שלך לא תומך בווידאו.
+      </video>
+
+      <div className="form-check d-flex align-items-center justify-content-center">
+        <input
+          className="form-check-input me-2"
+          type="checkbox"
+          id="confirmCheck"
+          checked={checked}
+          onChange={(e) => setChecked(e.target.checked)}
+        />
+        <label className="form-check-label" htmlFor="confirmCheck">
+         I confirm that I have successfully completed the calibration.
+        </label>
+      </div>
+
+      <button className="btn btn-primary mt-3" disabled={!checked} onClick={NextPage}>
+        Next
+      </button>
+    </div>
+  );
 }
 
 export default ExplanatoryV
