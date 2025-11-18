20d88ef17e6640f6a6f66c58d4e5ea2b13036a20
Ben7767 <142659132+Ben7767@users.noreply.github.com>
2025-11-18 16:26:55 +0200

    no Logo

--- src/componentsClient/Calibration.jsx
+++ src/componentsClient/Calibration.jsx
@@ -10,7 +10,7 @@ function Calibration() {
     <div className="d-flex justify-content-center align-items-center px-3" >
       <div className="bg-white p-5  text-center" style={{ maxWidth: '75%', width: '100%', borderRadius: '16px', maxHeight: '95vh', }}>
         <div className="mb-3">
-          <img style={{ height: '60px', width: '60px', borderRadius: '50px' }} src="src/assets/react.svg" alt="logo" />
+          {/* <img style={{ height: '60px', width: '60px', borderRadius: '50px' }} src="src/assets/react.svg" alt="logo" /> */}
         </div>
         <h3 className="py-2">Media Device Check</h3>
         <div className=" mx-auto" style={{ width: '65%', overflow: 'hidden', borderRadius: '24px' }}>
