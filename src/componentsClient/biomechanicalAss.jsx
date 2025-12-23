9e24d498711245ee556399289c60d5529cf89f96
Menachem49 <Menachem8683@gmail.com>
2025-12-23 20:11:05 +0200

    me

--- src/componentsClient/biomechanicalAss.jsx
+++ src/componentsClient/biomechanicalAss.jsx
@@ -224,7 +224,7 @@ function BiomechanicalAss() {
           <img src={logo} alt="Logo" width="24" />
           <span className="fw-bold fs-5 text-dark">Fitwave</span>
         </div>
-        <button  className="btn btn-link text-decoration-none text-secondary fw-bold" style={{ fontSize: '0.95rem' }}>
+        <button  onClick={() => nav("/HomeClient")} className="btn btn-link text-decoration-none text-secondary fw-bold" style={{ fontSize: '0.95rem' }}>
           Exit Test
         </button>
       </nav>
