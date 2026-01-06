7f9966430b87b3045aa04853c49eb4b4d90d3bed
Ben7767 <142659132+Ben7767@users.noreply.github.com>
2026-01-06 18:35:19 +0200

    changeblack

--- src/componentsClient/Calibration.jsx
+++ src/componentsClient/Calibration.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import thisIcon from '../assets/icon.png'; 
 import { FilesetResolver, PoseLandmarker } from '@mediapipe/tasks-vision';
 
+
 const HomeIcon = () => <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>;
 
 const styles = {
